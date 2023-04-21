d6554365c7d4e386a1b7477a6cb0b09bc24bdbfd
ZTrainWilliams <30279504+ZTrainWilliams@users.noreply.github.com>
2023-04-21 11:44:56 +0800

    fix: sliceIndex = 3

--- deptNews.js
+++ deptNews.js
@@ -37,7 +37,7 @@ const hasAds = (title) => {
 };
 
 const curDate = dayjs().format("YYYY-MM-DD");
-let sliceIndex = 5; //文章数量
+let sliceIndex = 3; //文章数量
 
 const getNews = () => {
   const promiseAll = Promise.all([
@@ -317,10 +317,11 @@ function formatSendStr(list) {
     if (item && item.list?.length > 0) {
       str += `## ${item.title}`;
       item.list.slice(0, sliceIndex).map((item, index) => {
-        str += `\n${index + 1}、[${item.title}](${item.link}) \n`;
-        //    <font color="comment" >${item.time}  ${
-        //   item.articleAuthor
-        // }</font>
+        str += `\n${index + 1}、[${item.title}](${item.link})
+            <font color="comment" >${
+           item.articleAuthor
+         }</font>`;
+        // ${item.time}
       });
       str += `\n\n\n`;
     }
